e5ea639da049029857be5fb70b19e3ba4bcf1a53
DemonDev0666 <DemonDev0666@gmail.com>
2025-09-09 00:46:43 -0400

    Added First Custom Item
    Signed-off-by: DemonDev0666 <DemonDev0666@gmail.com>

--- src/main/java/com/demondev/test_mod/item/ModItems.java
+++ src/main/java/com/demondev/test_mod/item/ModItems.java
@@ -1,4 +1,20 @@
 package com.demondev.test_mod.item;
 
+import com.demondev.test_mod.TestMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TestMod.MOD_ID);
+
+    public static final DeferredItem<Item> TESTER = ITEMS.register("tester",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
